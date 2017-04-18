3aa5f8b6c9ab13a1fb95d15840a166b2377f8b71
Guimeus <techer_l@etna-alternance.net>
2017-04-18 10:24:16 +0200

    Test listview

--- app/src/main/java/com/example/guimeus_mbp/ticmobi/MainActivity.java
+++ app/src/main/java/com/example/guimeus_mbp/ticmobi/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.guimeus_mbp.ticmobi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+    ListView mListView;
+    String[] prenoms = new String[]{
+            "Antoine", "Benoit", "Cyril", "David", "Eloise", "Florent",
+            "Gerard", "Hugo", "Ingrid", "Jonathan", "Kevin", "Logan",
+            "Mathieu", "Noemie", "Olivia", "Philippe", "Quentin", "Romain",
+            "Sophie", "Tristan", "Ulric", "Vincent", "Willy", "Xavier",
+            "Yann", "Zoé"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.catList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, prenoms);
+        mListView.setAdapter(adapter);
     }
 }
